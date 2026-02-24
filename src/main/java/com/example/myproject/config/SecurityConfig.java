f640b30a467e513f95dc0d1d7670f22bc4c74b85
Issac Jeferry <issacjeferry2006@gmail.com>
2026-02-24 21:55:04 +0530

    config modified

--- src/main/java/com/example/myproject/config/SecurityConfig.java
+++ src/main/java/com/example/myproject/config/SecurityConfig.java
@@ -59,13 +59,15 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+
+        configuration.setAllowedOriginPatterns(List.of("*")); // ✅ allow all
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false); // IMPORTANT when using *
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
+
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
